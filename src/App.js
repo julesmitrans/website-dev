11251ac29f0e3552aaac96e65ca9134a521cdd23
julesmitrans <juliamitranoo@gmail.com>
2021-09-23 16:44:50 -0500

    ?

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi Aidan was good mf?
+          Julia Rose Mitrano
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I love you xx
+           JavaScript React
         </a>
       </header>
     </div>
