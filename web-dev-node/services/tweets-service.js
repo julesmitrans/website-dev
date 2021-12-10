6b07dbee48d8bb5fd57412b083385427ffc6e372
julesmitrans <43657149+julesmitrans@users.noreply.github.com>
2021-12-10 09:17:15 -0500

    first commit

--- web-dev-node/services/tweets-service.js
+++ web-dev-node/services/tweets-service.js
@@ -46,5 +46,32 @@ const likeTweet = (req, res) => {
 }
 app.put('/api/tweets/:id/like', likeTweet);
 
+const postNewTweet = (req, res) => {
+    const newTweet = {
+        _id: (new Date()).getTime() + '',
+        "topic": "Web Development",
+        "userName": "ReactJS",
+        "verified": false,
+        "handle": "ReactJS",
+        "time": "2h",
+        "avatar-image": "../../../images/react-blue.png",
+        "logo-image": "../../../images/react-blue.png",
+        "stats": {
+            "comments": 123,
+            "retweets": 234,
+            "likes": 345
+        },
+        ...req.body,
+    }
+    tweets = [
+        newTweet,
+        ...tweets
+    ];
+    res.json(newTweet);
+}
+
+app.post('/api/tweets', createTweet);
+
+
 
 
